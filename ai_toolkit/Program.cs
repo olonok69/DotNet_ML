d968cc308bcbb91790967b27a14cf30a3d73faaf
Juan H <olonok@gmail.com>
2025-01-14 08:00:28 +0000

    ai toolkit

---
+++ ai_toolkit/Program.cs
@@ -0,0 +1,30 @@
+﻿// Program.cs
+using Azure.AI.OpenAI;
+using OpenAI.Chat;
+using OpenAI;
+
+Uri localhostUri = new("http://localhost:5272/v1/chat/completions");
+
+OpenAIClientOptions clientOptions = new();
+clientOptions.AddPolicy(
+    new OverrideRequestUriPolicy(localhostUri),
+    Azure.Core.HttpPipelinePosition.BeforeTransport);
+OpenAIClient client = new(openAIApiKey: "unused", clientOptions);
+
+ChatCompletionsOptions options = new()
+{
+    DeploymentName = "Phi-3-mini-128k-cpu-int4-rtn-block-32-acc-level-4-onnx",
+    Messages =
+    {
+        new ChatRequestSystemMessage("You are a helpful assistant. Be brief and succinct."),
+        new ChatRequestUserMessage("What is the golden ratio?"),
+    }
+};
+
+StreamingResponse<StreamingChatCompletionsUpdate> streamingChatResponse
+    = await client.GetChatCompletionsStreamingAsync(options);
+
+await foreach (StreamingChatCompletionsUpdate chatChunk in streamingChatResponse)
+{
+    Console.Write(chatChunk.ContentUpdate);
+}
